227916fd8e097562fed008921440ca1363ee92d3
Ace <omarace95@gmail.com>
2021-10-24 14:59:02 -0700

    able to now send search term and request api and search results page and able to address values in data

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -1,66 +1,31 @@
 var resultContentEl= document.querySelector("#result-content")
 
-var APIKey = "api.openweathermap.org/data/2.5/weather?q="+resultContentEl+"&appid=f48c044c914b169326af2c0881fb64da"
+function loadData(){
+   var  parameters =  document.location.search
+   console.log(parameters);
+   qSearchTermValue=parameters.trim().replace("?q=","")
+   console.log(qSearchTermValue)
+
+   loadApi(qSearchTermValue)
+}
+
+loadData()
+
+function loadApi(qSearchTermValue){
 
 
-
-// function loadData(){
-//    var  parameters =  document.location.search.split("&")
-//       //parameters=["q=las%20vegas","format=websites"]
-
-//     //  parameters[0]=["q=las%20vegas"]
-//       //parameters[1]=["format=websites"]
-//    var qSearchTermValue= parameters[0].split("=")[1] //las vegas
-//   // qSearchTermValue="las%20vegas"
-  
-//    var formatValue=parameters[1].split("=")[1]
-//   // formatValue="websites"
-
-//    qSearchTermValue=qSearchTermValue.trim().replace("%20"," ")
-//    formatValue.trim().replace("%20"," ")
-//    console.log(qSearchTermValue)
-//    console.log(formatValue)
-
-
-//    loadApi(qSearchTermValue,formatValue)
-// }
-
-// loadData()
-
-// function loadApi(qSearchTermValue,formatValue){
-
-
-//     var locQueryUrl = 'https://www.loc.gov/search/?fo=json&q='+qSearchTermValue
-
-//    if(formatValue.length>0){
-//     locQueryUrl = 'https://www.loc.gov/' + formatValue + '/?fo=json&q='+qSearchTermValue
-
-//    }
+    var locQueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+qSearchTermValue+'&APPID=f48c044c914b169326af2c0881fb64da'
+    console.log(locQueryUrl);
    
-//     fetch(locQueryUrl)
-//     .then(function(response){
-//        return response.json()
-//     })
-//     .then(function(data){
-//       console.log(data.results)
+    fetch(locQueryUrl)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+        console.log(data.name);
+        console.log(data.main.temp);
+        console.log(data.weather[0].description);
 
-//       for (let i = 0; i < data.results.length; i++) {
-//          resultContentEl.innerHTML = resultContentEl.innerHTML+`
-//          <div class="card bg-light text-dark mb-3 p-3">
-//          <div class="card-body">
-//              <h3>${ data.results[i].title } </h3>
-//              <p><strong>Date:</strong>${data.results[i].timestamp} <br><strong>Subjects:</strong> 
-//               ${data.results[i].subject[0]}<br><strong>Description:</strong>  
-//               ${data.results[i].description}
-              
-//               </p><a href="${data.results[i].url}" class="btn btn-dark">Read More</a>
-//          </div>
-//      </div>
- 
-         
-//          `
-          
-//       }
-//     })
-
-// }
+    })
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ var handleForm = function(event){
       event.preventDefault();
       searchTerm = searchInputEl.value;
       console.log(searchTerm);
+      document.location.replace("search-results.html?q="+searchTerm)
       var urlRequest = 'https://api.openweathermap.org/data/2.5/weather?q='+searchTerm+'&APPID=f48c044c914b169326af2c0881fb64da'
       fetch(urlRequest)
       .then(function (response) {
@@ -13,6 +14,10 @@ var handleForm = function(event){
       })
       .then(function (data) {
         console.log(data);
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        console.log(lat);
+        console.log(lon);
       });
 }
 searchFormEl.addEventListener("submit", handleForm)
