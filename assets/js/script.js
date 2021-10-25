3ff3c083a7d98124261131bae5dadeef4026c5cd
Ace <omarace95@gmail.com>
2021-10-25 01:51:57 -0700

    removed search results html+js was originally going to divert to a new html page to emulate the previous mini project but I just feel like it will be a bit easier to have it be one page instead

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,6 @@ var handleForm = function(event){
       event.preventDefault();
       searchTerm = searchInputEl.value;
       console.log(searchTerm);
-      document.location.replace("search-results.html?q="+searchTerm)
       var urlRequest = 'https://api.openweathermap.org/data/2.5/weather?q='+searchTerm+'&APPID=f48c044c914b169326af2c0881fb64da'
       fetch(urlRequest)
       .then(function (response) {
@@ -18,6 +17,14 @@ var handleForm = function(event){
         var lon = data.coord.lon;
         console.log(lat);
         console.log(lon);
+        var urlOneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&APPID=f48c044c914b169326af2c0881fb64da'
+          fetch(urlOneCall)
+            .then(function (response) {
+            return response.json();
+            })
+            .then(function (data) {
+            console.log(data);
+          })
       });
 }
 searchFormEl.addEventListener("submit", handleForm)
